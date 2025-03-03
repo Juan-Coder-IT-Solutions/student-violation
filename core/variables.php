704a75688de97f23883ca785e13e3bfd651bb2c6
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2025-03-03 21:29:49 +0800

    Update variables.php

--- core/variables.php
+++ core/variables.php
@@ -4,15 +4,15 @@ define("view","pages/");
 
 /** Database connection **/
 
-define("host","localhost");
-define("username","root");
-define("password","");
-define("database","student_violations_db");
-
 // define("host","localhost");
-// define("username","u814036432_sv_root");
-// define("password","studentViolations123!");
-// define("database","u814036432_sv_db");
+// define("username","root");
+// define("password","");
+// define("database","student_violations_db");
+
+define("host","localhost");
+define("username","u814036432_sv_root");
+define("password","studentViolations123!");
+define("database","u814036432_sv_db");
 
 define("table","tbl_users");
 define("user_session_id","user_id");
